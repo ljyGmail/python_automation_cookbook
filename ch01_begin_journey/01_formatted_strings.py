f570560a4803c686cac35da9500aa2bf43c27c46
ljy <liangjy881@naver.com>
2023-04-15 01:09:07 +0900

    start: python automation cookbook

---
+++ ch01_begin_journey/01_formatted_strings.py
@@ -0,0 +1,4 @@
+print("Put the value of the string here: {}".format("STRING"))
+print("It can be any type ({}) and more than one ({})".format(1.23, "STRING"))
+print("Specify the order: {1}, {0}".format("first", "second"))
+print("Or name parameters: {first}, {second}".format(second="SECOND", first="FIRST"))
---
+++ ch01_begin_journey/03_more_formatting.py
@@ -0,0 +1,7 @@
+param1 = "first"
+param2 = "second"
+print(f"Parameters {param1}:{param2}")
+
+# To write a curly bracket, repeat it twice.
+value = "VALUE"
+print(f"This is the value, in curly brackets {{{value}}}")
