5f050d137005c7e8a4376a9e0a263570d73b961e
liangjinyong <liangjy881@gmail.com>
2023-05-23 14:26:41 +0900

    setting up a cron job

---
+++ ch02_automating_tasks_made_easy/03_cron.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+from datetime import datetime
+import configparser
+
+
+def main(number, other_number, output):
+    result = number * other_number
+    print(f'[{datetime.utcnow().isoformat()}] The result is {result}', file=output)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-c', '--config', type=argparse.FileType('r'), help='config file', default='ch02_automating_tasks_made_easy/config.ini')
+    parser.add_argument('-o', dest='output', type=argparse.FileType('a'),
+                        help='output file', default=sys.stdout)
+
+    args = parser.parse_args()
+    if args.config:
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
+        # Transforming values into integers
+        args.n1 = int(config['ARGUMENTS']['n1'])
+        args.n2 = int(config['ARGUMENTS']['n2'])
+
+    main(args.n1, args.n2, args.output)
